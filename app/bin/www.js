19d9ee9de73e214765bc6098e46f0c27dea08a3f
kIMSEOKJIN <rinkis@naver.com>
2023-12-03 18:39:29 +0900

    프런트 단 javjascript파일 만들기

---
+++ app/bin/www.js
@@ -0,0 +1,9 @@
+"use strict";
+const app = require("../app");
+/*app.listen(PORT,function(){
+    console.log("서버가동");
+});*/
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log("서버가동");
+});
